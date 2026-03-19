ab141efdbcf36573a517a56186b1e879481c885a
Yakov <yaxa499@gmail.com>
2026-03-19 14:41:06 +0300

    feat: добавил методы парсеру валют

--- lib/cbr_converter.rb
+++ lib/cbr_converter.rb
@@ -1,9 +1,40 @@
 # frozen_string_literal: true
 
+
 require_relative "cbr_converter/version"
 require_relative "cbr_converter/currency_parser"
 
 module CbrConverter
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.current_currency_rates
+    @current_currency_rates ||= begin
+      parser = CurrencyParser.new.parse_rates
+      parser["RUB"] = BigDecimal("1.0")
+      parser
+    end
+  end
+
+  def self.get_currency_rate(currency)
+    rates = current_currency_rates
+
+    raise Error, "Валюта (#{currency}) не найдена в данных ЦБ" unless rates[currency]
+
+    rates[currency].truncate(2)
+  end
+
+  def self.compare_currencies(first_currency, second_currency)
+    first_rate = get_currency_rate(first_currency)
+    second_rate = get_currency_rate(second_currency)
+
+    (first_rate / second_rate).truncate(2)
+  end
+
+  def self.available_currencies
+    current_currency_rates.keys.sort
+  end
+
+  def self.refresh_rates!
+    @current_currency_rates = nil
+  end
 end
